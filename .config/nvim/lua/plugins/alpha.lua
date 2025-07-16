179bf1d96456e96a8e0c94ee7736271a6e1d14ec
satbunch <bump.satomi2331@gmail.com>
2025-07-16 17:18:51 +0900

    feat add auto-session

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -19,10 +19,14 @@ return {
 
     -- Set menu
     dashboard.section.buttons.val = {
-      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+      dashboard.button("f", "  Find File", ":Telescope find_files<CR>"),
+      dashboard.button("n", "  New File", ":ene | startinsert<CR>"),
+      dashboard.button("g", "  Find Text", ":Telescope live_grep<CR>"),
+      dashboard.button("r", "  Recent Files", ":Telescope oldfiles<CR>"),
+      dashboard.button("c", "  Config", ":Telescope find_files cwd=" .. vim.fn.stdpath("config") .. "<CR>"),
+      dashboard.button("s", "  Restore Session", ":SessionRestore<CR>"),
+      dashboard.button("l", "󰒲  Lazy", ":Lazy<CR>"),
+      dashboard.button("q", "  Quit", ":qa<CR>"),
     }
 
     -- Send config to alpha
---
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -0,0 +1,30 @@
+return {
+  "rmagatti/auto-session",
+  config = function()
+    local auto_session = require("auto-session")
+
+    auto_session.setup({
+      auto_restore_enabled = false,
+      auto_session_suppress_dirs = {
+        "~", -- ホーム直下
+        "~/Desktop",
+        "~/Documents",
+        "~/Downloads",
+        "~/Dropbox",
+        "~/Music",
+        "~/Pictures",
+        "~/Videos",
+        "~/Public",
+        "~/Templates",
+        "~/snap",
+        "~/Postman",
+        "~/alacritty",
+      }
+    })
+
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" })
+    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" })
+  end,
+}
